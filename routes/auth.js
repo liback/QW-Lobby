573a6e0dade87b4ad4c29c88064371db16f084fd
Linus Bäckman <linus@refragged.com>
2016-06-21 20:21:54 +0200

    Added login page.

--- routes/auth.js
+++ routes/auth.js
@@ -2,11 +2,25 @@ var express = require('express');
 var router = express.Router();
 var passport = require('passport');
 
-/* GET users listing. */
+
+// Login page
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Login', message: req.flash('loginMessage') });
+});
+
+// Login request handling
+router.post('/login', passport.authenticate('local-login', {
+    successRedirect:    '/lobby/',
+    failureRedirect:    '/auth/login/',
+    failureFlash:       true
+}));
+
+// Signup page
 router.get('/signup', function(req, res, next) {
   res.render('signup', { title: 'Sign up', message: req.flash('signupMessage') });
 });
 
+// Signup request handling
 router.post('/signup', passport.authenticate('local-signup', {
     successRedirect:    '/auth/success',
     failureRedirect:    '/auth/signup',
@@ -17,5 +31,7 @@ router.post('/signup', passport.authenticate('local-signup', {
 router.get('/success', function(req, res, next) {
   res.render('auth/success', { title: 'Successfully signed up' });
 });
+
+
 
 module.exports = router;
