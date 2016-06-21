d0ff708573c86bc0e10897a022a81815d2d16b4c
Linus Bäckman <linus@refragged.com>
2016-06-21 18:37:31 +0200

    Fixed signup route.

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/signup', function(req, res, next) {
-  res.render('signup', { title: 'Sign up' });
+  res.render('signup', { title: 'Sign up', message: req.flash('signupMessage') });
 });
 
 module.exports = router;
