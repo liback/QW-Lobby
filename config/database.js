a0d9bb250ddac428fb73e2c10acf1f1fab69f8c8
Linus Bäckman <linus@refragged.com>
2016-06-21 15:26:13 +0200

    Created database config

---
+++ config/database.js
@@ -0,0 +1,9 @@
+module.exports = {
+    'connection': {
+        'host': 'localhost',
+        'user': 'root',
+        'password': 'root'
+    },
+    'database': 'qwlobby',
+    'users_table': 'users'
+};
